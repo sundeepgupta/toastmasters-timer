8fb3155d46206672fe39e82da400cefc7dea7532
Sundeep Gupta <sundeep@sundeepgupta.ca>
2014-02-22 13:37:44 -0500

    Installed CocoaPods, Kiwi, iRate. Kiwi is working.

---
+++ Podfile
@@ -0,0 +1,12 @@
+# Podfile
+
+platform :ios, "7.0"
+
+#Supress warnings from all pods.
+inhibit_all_warnings!
+
+target :"Toastmasters Timer Tests", :exclusive => true do
+   pod "Kiwi/XCTest"
+end
+
+pod "iRate"
---
+++ Toastmasters Timer/LocalNotificationsSpec.m
@@ -0,0 +1,15 @@
+#import "Kiwi.h"
+
+
+SPEC_BEGIN(LocalNotificationsSpec)
+
+describe(@"LocalNotifications", ^{
+    
+    context(@"", ^{
+        
+        it(@"test test", ^{
+        });
+    });
+});
+
+SPEC_END
